94f49525ab382ab5ca7faef3c5749d34a87ba940
alexjnelson <alexj.nelson9@gmail.com>
2023-03-22 17:14:26 -0400

    implemented routing functionality and reworked structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import NavBar from './components/Navbar';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Footer from './components/Footer';
+
 function App() {
     return (
         <>
-            <NavBar />
-            <Footer />
+
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+import NavBar from "./components/Navbar";
+import Footer from "./components/Footer";
+import App from "./App";
+import About from "./pages/About";
+import Services from "./pages/Services";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "services",
+        element: <Services />,
+      },
+    ],
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <NavBar />
+    <RouterProvider router={router} />
+    <Footer />
   </React.StrictMode>
 );
 
