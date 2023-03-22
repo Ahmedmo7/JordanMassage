e1a7e4563737484ca895a216e2ea5b0bbd7b9e30
Ahmed Mohamed <mohamed.ahmedelser23@gmail.com>
2023-03-22 16:38:11 -0400

    footer commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import NavBar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import Footer from './components/Footer';
 function App() {
     return (
         <>
             <NavBar />
+            <Footer />
         </>
     )
 }
