b24bd9eee53ea7e544d5bf0997141fa39bc02208
alexjnelson <alexj.nelson9@gmail.com>
2023-03-22 16:40:33 -0400

    updated spacing and added logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,46 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Button } from "react-bootstrap";
 
 function NavBar() {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container>
-                <Navbar.Brand href="#home">Jordan Brisson</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">About</Nav.Link>
-                        <Nav.Link href="#home">Modalities</Nav.Link>
-                        <a className="btn btn-dark" href="https://www.picktime.com/mobile" target="_blank" rel="noreferrer">
-                            Booking Link
-                        </a>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="light" expand="lg" fixed="top" className="p-4">
+      <Navbar.Brand href="#home">
+        <img width="120" height="auto" src="logo.png" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="ml-auto" />
+
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav.Link href="#home" className="px-4 py-2 py-lg-0">
+          Home
+        </Nav.Link>
+        <Nav.Link href="#link" className="px-4 py-2 py-lg-0">
+          About
+        </Nav.Link>
+        <Nav.Link href="#home" className="px-4 py-2 py-lg-0">
+          Services
+        </Nav.Link>
+
+        <Button
+          className="d-lg-none w-100 my-2"
+          variant="dark"
+          href="https://www.picktime.com/mobile"
+          target="_blank"
+        >
+          Book Now
+        </Button>
+      </Navbar.Collapse>
+
+      <Button
+        className="ml-auto d-none d-lg-block"
+        variant="dark"
+        href="https://www.picktime.com/mobile"
+        target="_blank"
+      >
+        Book Now
+      </Button>
+    </Navbar>
+  );
 }
 
 export default NavBar;
