30d1926dd07ad26b280718edaed6d78b64a3d0aa
Ahmed Mohamed <mohamed.ahmedelser23@gmail.com>
2024-02-22 22:56:05 -0500

    fix paragraph

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
     const [isMobile, setIsMobile] = useState(false);
 
     useEffect(() => {
-        // Check if the window size is mobile on initial mount and when it's resized
         function handleResize() {
             setIsMobile(window.innerWidth <= 767);
         }
@@ -19,42 +18,27 @@ function App() {
     }, []);
 
     return (
-        <div className="p-5">
+        <div className="p-12">
             <Container>
                 <Row>
-                    {/* On mobile, move the title to the top */}
-                    {isMobile && (
-                        <Col xs={12} className="text-center mobile-title mb-4">
-                            <h1>Jordan Brisson Mobile Massage Therapy</h1>
-                            <h4 style={{ fontStyle: "italic" }}>Your home, your haven.</h4>
-                        </Col>
-                    )}
-                    {/* On larger screens, show the image on the left */}
-                    <Col md={5}>
-                        <br></br>
-                        <br></br>
-                        <br></br>
+                    {/* Image always on top */}
+                    <center> <Col xs={8} className="text-center mb-12 p-4 ">
                         <Image
                             src="/images/Outside1.jpg"
                             alt="Photo of "
                             fluid
-                            className={`rounded shadow intro-image ${isMobile ? "mb-4" : ""}`}
+                            className="rounded shadow intro-image"
                         />
+                    </Col></center>
+                    {/* Title for mobile and larger screens */}
+                    <Col xs={12} className="text-center mb-4">
+                        <h1>Jordan Brisson Mobile Massage Therapy</h1>
+                        <h4 style={{ fontStyle: "italic" }}>Your home, your haven.</h4>
                     </Col>
-                    {/* On larger screens, show the title and content on the right */}
-                    <Col md={7}>
-                        {!isMobile && (
-                            <div className="d-none d-md-block">
-                                <h1>Jordan Brisson Mobile Massage Therapy</h1>
-                                <center><h4 style={{ fontStyle: "italic" }}>Your home, your haven.</h4></center>
-                                <br></br>
-                            </div>
-                        )}
-                        {/* The rest of the content */}
+                    {/* Main content */}
+                    <Col xs={12}>
                         <p>
-                            As a <strong>Registered Massage Therapist</strong>, I strive to deliver an exceptional massage that targets your unique needs. Whether you are an athlete, an office worker, a person who lacks mobility, or someone who needs to destress, your massage and treatment plan will be <strong>designed for you.</strong>                         </p>
-                        <p>
-                            As a massage therapist, the most important tool at my disposal is the power of listening. Understanding my client and their goals, assessing their needs, and applying manual therapies all require listening, whether it be with my ears or with my hands.
+                            As a <strong>Registered Massage Therapist</strong>, I strive to deliver an exceptional massage that targets your unique needs. Whether you are an athlete, an office worker, a person who lacks mobility, or someone who needs to destress, your massage and treatment plan will be <strong>designed for you.</strong>
                         </p>
                         <p>
                             I offer various massage techniques, including Swedish, deep tissue, fascial massage, stretch therapy (LVL 1 FST), trigger point therapy, lymphatic drainage, and more! I bring the massage table and linens, <strong>so all you need is an open space.</strong>
